a75641302942e194fd646ff9690c319d0d6e663d
aidadieudonne <aidaharing10@gmail.com>
2025-03-05 14:43:02 -0700

    practicing TDD

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,34 @@
 // Your tests here
+import { isPalindrome } from "../utils";
+
+describe("isPalindrome", () => {
+    it("returns true if the word is a palindrome", () => {
+        const word = "racecar";
+        const answer = isPalindrome(word);
+
+        expect(answer).toBe(true);
+    });
+
+    it("returns false when the word is not a palindrome", () => {
+
+        const word = "car";
+        const answer = isPalindrome(word);
+
+        expect(answer).toBe(false);
+    });
+
+    it("returns true for words that are a combination of uppercase and lowercase letters", () => {
+        const word = "rAcECar"
+        const answer =  isPalindrome(word)
+
+        expect(answer).toBe(true)
+    });
+
+    it("returns an error if there is an empty string", () => {
+        const word = "";
+        
+        expect(() => isPalindrome(word)).toThrow();
+    });
+
+    }
+)
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,14 @@
-// Your code here
+
+function reverse(string) {
+    return string.split("").reverse().join("");
+}
+
+export function isPalindrome(word) {
+    if  (!/^[A-Za-z]+$/.test(word)) {
+        throw new Error("Invalid Input");
+    }
+
+    const lowerCaseWord = word.toLowerCase();
+    return lowerCaseWord === reverse(lowerCaseWord);
+}
+
